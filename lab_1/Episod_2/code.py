7b3711c41af849f4bec2c3005c144f99e644813d
FastBery <Falls.ignatev@mail.ru>
2023-10-20 13:50:20 +0300

    added Episode 2

---
+++ lab_1/Episod_2/code.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+N = 0
+with open('data.dat', 'r') as f:
+   for line in f:
+    N += 1
+N = int(N/2)
+fig, axs = plt.subplots(nrows=int(N/2), ncols=2, dpi=100)
+
+data = []
+
+with open('data.dat', 'r') as f:
+    data = [i.replace('\n','') for i in f.readlines()]
+
+axs = axs.flatten()
+maxy = 0
+maxx = 0
+minx = 0
+miny = 0
+
+for i in range(N):
+    datax = [float(j) for j in data[2*i].split()]
+    if(max(datax) > maxx):
+       maxx = max(datax)
+    if(min(datax) < minx):
+       minx = min(datax)
+    datay = [float(j) for j in data[2*i + 1].split()]
+    if(max(datay) > maxy):
+       maxy = max(datay)
+    if(min(datay) < miny):
+       miny = min(datay)
+    Frame = 'Frame' + str(i)
+    axs[i].plot(datax, datay)
+    axs[i].set_title(Frame)
+
+for i in axs:
+    i.set_xlim(minx, maxx)
+    i.set_ylim(miny, maxy)
+    i.grid(linestyle='-')
+    i.set_xticks(np.arange(minx, maxx, step=1.5))
+    i.set_yticks(np.arange(miny, maxy, step=(maxy - miny)/10))
+
+plt.show()
